f6638ce351b2f1321afa1ea53d139b1c5739100d
jhoel silvestre <jhoel.sv25@gmail.com>
2023-11-19 22:08:27 -0500

    FIX cart pages completed

--- src/app/shopping/layouts/side-cart/side-cart.component.ts
+++ src/app/shopping/layouts/side-cart/side-cart.component.ts
@@ -2,13 +2,14 @@ import { Component, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardPreviewComponent } from '../../components/card-preview/card-preview.component';
 import { CartService } from '../../services/cart.service';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'side-cart',
   standalone: true,
   templateUrl: './side-cart.component.html',
   styleUrl: './side-cart.component.css',
-  imports: [CommonModule, CardPreviewComponent],
+  imports: [CommonModule, CardPreviewComponent, RouterLinkWithHref],
 })
 export class SideCartComponent {
   private cartService = inject(CartService);
--- src/app/shopping/pages/cart/cart.component.ts
+++ src/app/shopping/pages/cart/cart.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CartService } from '../../services/cart.service';
+import { Product } from '../../models/product';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLinkWithHref],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
+  styleUrl: './cart.component.css',
 })
 export class CartComponent {
+  private cartService = inject(CartService);
+  public carts = this.cartService.cart;
+  public total = this.cartService.total;
 
+  updateQuantity(item: Product, quantity: string) {
+    this.cartService.updateQuantity(item, Number(quantity));
+  }
+
+  removeItem(item: Product) {
+    this.cartService.removeFromCart(item);
+  }
 }
