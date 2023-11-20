b5c78519d1902f4743ed92d2b6250eb3953d7644
jhoel silvestre <jhoel.sv25@gmail.com>
2023-11-19 19:29:57 -0500

    add layouts

---
+++ src/app/shopping/layouts/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, computed, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SideCartComponent } from '../side-cart/side-cart.component';
+import { CartService } from '../../services/cart.service';
+import { RouterLinkWithHref } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+  imports: [CommonModule, SideCartComponent, RouterLinkWithHref],
+})
+export class HeaderComponent {
+  public isOpen = signal<boolean>(true);
+  private cartService = inject(CartService);
+  public cart = computed(() => this.cartService.cart());
+  toggleCart() {
+    this.isOpen.update((preState) => !preState);
+  }
+}
