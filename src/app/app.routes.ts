155718be9fa45808d10aee47b1de29588fa81fb5
jhoel silvestre <jhoel.sv25@gmail.com>
2023-11-19 19:29:17 -0500

    initial proyect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './shopping/pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('./shopping/pages/cart/cart.component').then(
+        (m) => m.CartComponent
+      ),
+  },
+  {
+    path: 'favorites',
+    loadComponent: () =>
+      import('./shopping/pages/favorite/favorite.component').then(
+        (m) => m.FavoriteComponent
+      ),
+  },
+
+  {
+    path: 'product/:id',
+    loadComponent: () =>
+      import(
+        './shopping/components/product-detail/product-detail.component'
+      ).then((m) => m.ProductDetailComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
